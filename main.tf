dceaa0a6fcaf19ca3dbbc95d77ca7882ad18e194
tcancissu <thiago.cancissu@gmail.com>
2024-08-18 22:41:46 -0300

    Inicializando repositorio

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+  key_name = "key-rsa"
+
+  tags = {
+    Name = "AppServerInstance01"
+  }
+}
