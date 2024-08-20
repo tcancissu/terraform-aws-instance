1c45abf155a8195e5050d05a1ceba09fcd299046
tcancissu <thiago.cancissu@gmail.com>
2024-08-19 21:34:52 -0300

    utilizando o user-data

--- main.tf
+++ main.tf
@@ -17,8 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   key_name = "key-rsa"
-
+  user_data = <<-EOF
+                  #!/bin/bash
+                  cd /home/ubuntu
+                  echo "<h1>Feito com Terraform</h1>" > index.html
+                  nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
-    Name = "AppServerInstance01"
+    Name = "AppServerInstance"
   }
 }
